7eaf5e5331079ff9b86175727223328a3fa3deba
YeBin0711 <ayb0711@sookmyung.ac.kr>
2024-08-03 16:55:30 +0900

    Fix 좋아요 버튼

--- src/pages/review/ReviewModal.js
+++ src/pages/review/ReviewModal.js
@@ -1,39 +1,48 @@
-import React, { useState, useEffect } from 'react';
-import Modal from 'react-modal';
-import axios from 'axios';
-import { reviewModalStyles } from '../../styles/reviewModalStyles';
-import LikeButton, { useToggleLike } from '../../components/LikeButtonComponents';
-import { refreshAccessToken } from '../../components/refreshAccess';
+import React, { useState, useEffect } from "react";
+import Modal from "react-modal";
+import axios from "axios";
+import { reviewModalStyles } from "../../styles/reviewModalStyles";
+import LikeButton from "../../components/LikeButtonComponents";
+import { refreshAccessToken } from "../../components/refreshAccess";
 
 const formatDate = (dateString) => {
   const date = new Date(dateString);
   const year = date.getFullYear();
-  const month = String(date.getMonth() + 1).padStart(2, '0');
-  const day = String(date.getDate()).padStart(2, '0');
-  const hours = String(date.getHours()).padStart(2, '0');
-  const minutes = String(date.getMinutes()).padStart(2, '0');
+  const month = String(date.getMonth() + 1).padStart(2, "0");
+  const day = String(date.getDate()).padStart(2, "0");
+  const hours = String(date.getHours()).padStart(2, "0");
+  const minutes = String(date.getMinutes()).padStart(2, "0");
 
   return `${year}. ${month}. ${day}. ${hours}:${minutes}`;
 };
 
-const ReviewModal = ({ isOpen, review, closeModal, openEditModal, deleteReview, onLikeToggle }) => {
+const ReviewModal = ({
+  isOpen,
+  review,
+  closeModal,
+  openEditModal,
+  deleteReview,
+  onLikeToggle,
+  fetchReviewDetails,
+}) => {
   const [profileData, setProfileData] = useState(null);
   const [error, setError] = useState(null);
-  const { liked, likeCount, toggleLike, error: likeError } = useToggleLike(review.postId, review.likes, review.likeCnt);
 
   useEffect(() => {
     if (isOpen) {
       const fetchProfileData = async () => {
         try {
-          const accessToken = localStorage.getItem('accessToken');
-          
+          const accessToken = localStorage.getItem("accessToken");
           if (!accessToken) {
             return;
           }
-          
-          const response = await axios.get(`${process.env.REACT_APP_API_URL}/api/member/profile`, {
-            headers: { Authorization: `Bearer ${accessToken}` },
-          });
+
+          const response = await axios.get(
+            `${process.env.REACT_APP_API_URL}/api/member/profile`,
+            {
+              headers: { Authorization: `Bearer ${accessToken}` },
+            }
+          );
 
           const data = response.data;
           setProfileData({
@@ -41,7 +50,7 @@ const ReviewModal = ({ isOpen, review, closeModal, openEditModal, deleteReview, 
             profileImage: data.profileImageUrl,
           });
         } catch (error) {
-          setError('프로필 정보를 불러오는 중 에러 발생');
+          setError("프로필 정보를 불러오는 중 에러 발생");
         }
       };
 
@@ -55,52 +64,63 @@ const ReviewModal = ({ isOpen, review, closeModal, openEditModal, deleteReview, 
   };
 
   const handleDeleteClick = async () => {
-    if (window.confirm('삭제하시겠습니까?')) {
+    if (window.confirm("삭제하시겠습니까?")) {
       try {
-        const accessToken = localStorage.getItem('accessToken');
-        await axios.delete(`${process.env.REACT_APP_API_URL}/api/posts/${review.postId}`, {
-          headers: { Authorization: `Bearer ${accessToken}` },
-        });
+        const accessToken = localStorage.getItem("accessToken");
+        await axios.delete(
+          `${process.env.REACT_APP_API_URL}/api/posts/${review.postId}`,
+          {
+            headers: { Authorization: `Bearer ${accessToken}` },
+          }
+        );
         deleteReview(review);
         closeModal();
       } catch (error) {
-        if (error.response?.status === 401 && error.response?.data?.error === 'access_token_expired') {
+        if (
+          error.response?.status === 401 &&
+          error.response?.data?.error === "access_token_expired"
+        ) {
           try {
-            const refreshToken = localStorage.getItem('refreshToken');
+            const refreshToken = localStorage.getItem("refreshToken");
             if (!refreshToken) {
-              throw new Error('No refresh token available.');
+              throw new Error("No refresh token available.");
             }
             const newAccessToken = await refreshAccessToken(refreshToken);
             const headers = { Authorization: `Bearer ${newAccessToken}` };
-            await axios.delete(`${process.env.REACT_APP_API_URL}/api/posts/${review.postId}`, {
-              headers,
-            });
+            await axios.delete(
+              `${process.env.REACT_APP_API_URL}/api/posts/${review.postId}`,
+              {
+                headers,
+              }
+            );
             deleteReview(review);
             closeModal();
           } catch (refreshError) {
-            console.error('Failed to refresh access token:', refreshError);
-            setError('토큰 갱신 중 오류가 발생하였습니다. 다시 시도해 주세요.');
+            console.error("Failed to refresh access token:", refreshError);
+            setError("토큰 갱신 중 오류가 발생하였습니다. 다시 시도해 주세요.");
           }
         } else {
           if (error.response) {
             switch (error.response.status) {
               case 401:
-                setError('로그인이 필요합니다.');
+                setError("로그인이 필요합니다.");
                 break;
               case 404:
-                setError('해당 게시글이 존재하지 않습니다.');
+                setError("해당 게시글이 존재하지 않습니다.");
                 break;
               case 403:
-                setError('해당 게시글을 삭제할 권한이 없습니다. 본인이 작성한 글만 삭제할 수 있습니다.');
+                setError(
+                  "해당 게시글을 삭제할 권한이 없습니다. 본인이 작성한 글만 삭제할 수 있습니다."
+                );
                 break;
               case 500:
-                setError('게시글 삭제 중 오류가 발생하였습니다.');
+                setError("게시글 삭제 중 오류가 발생하였습니다.");
                 break;
               default:
                 setError(`오류가 발생하였습니다: ${error.message}`);
             }
           } else if (error.request) {
-            setError('서버와 연결할 수 없습니다.');
+            setError("서버와 연결할 수 없습니다.");
           } else {
             setError(`오류가 발생하였습니다: ${error.message}`);
           }
@@ -111,8 +131,8 @@ const ReviewModal = ({ isOpen, review, closeModal, openEditModal, deleteReview, 
 
   const handleLike = async (e) => {
     e.stopPropagation();
-    await toggleLike();
-    onLikeToggle(review.postId, !liked);
+    await onLikeToggle(review.postId);
+    await fetchReviewDetails(review.postId);
   };
 
   const photos = Array.isArray(review.images) ? review.images : [];
@@ -163,8 +183,14 @@ const ReviewModal = ({ isOpen, review, closeModal, openEditModal, deleteReview, 
       </div>
       <div style={reviewModalStyles.footer}>
         <div style={reviewModalStyles.likeSection}>
-          <LikeButton liked={liked} toggleLike={handleLike} likeCount={likeCount} />
-          <span style={reviewModalStyles.like}>좋아요 {likeCount.toString()}</span>
+          <LikeButton
+            liked={review.likes}
+            toggleLike={handleLike}
+            likeCount={review.likeCnt}
+          />
+          <span style={reviewModalStyles.like}>
+            좋아요 {review.likeCnt.toString()}
+          </span>
         </div>
         <div style={reviewModalStyles.tags}>
           {review.postHashtags.map((tag, index) => (
